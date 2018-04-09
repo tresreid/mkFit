9b49f525a37daf3d8196b96a5a2315d0c749fd49
Brian J Gravelle <gravelle@cs.uoregon.edu>
2018-04-09 13:55:47 -0700

    scaling script

--- run_scaling.py
+++ run_scaling.py
@@ -9,46 +9,48 @@ or ...TTbar70_cache
 import os
 import subprocess as sp
 
-# Grover
-'''
-TAU_DIR = '/home/users/gravelle/taucmdr-enterprise-2/system/tau/tau-2.27'
-ENV  = [('TAU_MAKEFILE', TAU_DIR + '/x86_64/lib/Makefile.tau-93948b87-icpc-papi-tbb'), \
-('TAU_ROOT', TAU_DIR)] 
-PATH=('PATH', TAU_DIR + '/x86_64/bin')
-'''
+machine = 'Talapas'
 
-# Talapas
-TAU_DIR = '/projects/hpcl/shared/taucmdr-enterprise/system/tau/tau-2.27/'
+if machine == 'Grover':
+	TAU_DIR = '/home/users/gravelle/taucmdr-enterprise-2/system/tau/tau-2.27'
+	ENV  = [('TAU_MAKEFILE', TAU_DIR + '/x86_64/lib/Makefile.tau-93948b87-icpc-papi-tbb'), \
+	('TAU_ROOT', TAU_DIR)] 
+	PATH=('PATH', TAU_DIR + '/x86_64/bin')
+	TAU = 'tau_exec -T 93948b87,tbb,papi,serial,icpc -ebs {:s}'
+	CMD  = './mkFit/mkFit --cmssw-n2seeds --input-file {:s} --build-ce --num-thr {:d} --num-events {:d} --silent'
+	SLURM = False
 
-ENV  = [('TAU_MAKEFILE', TAU_DIR + '/x86_64/lib/ Makefile.tau-05c9fa46-icpc-papi-tbb'), \
-('TAU_ROOT', TAU_DIR),\
-('TAU_CALLPATH','1'),\
-('TAU_CALLPATH_DEPTH','100'),\
-('TAU_UNWIND','1')] 
 
-PATH=('PATH', TAU_DIR + '/x86_64/bin')
+if machine == 'Talapas':
+	TAU_DIR = '/projects/hpcl/shared/taucmdr-enterprise/system/tau/tau-2.27/'
+	ENV  = [('TAU_MAKEFILE', TAU_DIR + '/x86_64/lib/ Makefile.tau-05c9fa46-icpc-papi-tbb'), \
+	('TAU_ROOT', TAU_DIR),\
+	('TAU_CALLPATH','1'),\
+	('TAU_CALLPATH_DEPTH','100'),\
+	('TAU_UNWIND','1')] 
+	CMD  = 'mkFit --cmssw-n2seeds --input-file {:s} --build-ce --num-thr {:d} --num-events {:d} --silent'
+	TAU = 'tau_exec -T 05c9fa46,tbb,papi,serial,icpc -ebs {:s}'
+	SLURM = True
 
 
 # define where the data will go
 DIR  = '.tau/mictest_sampling/'
 
 # define which simulation file to use
-FILE = 'TTbar35PU-memoryFile.fv3.clean.writeAll.recT.072617.bin'
+FILE_DIR = '/projects/hpcl/bgravell/mictest_sampling/'
+FILE35 = 'TTbar35PU-memoryFile.fv3.clean.writeAll.recT.072617.bin'
+FILE70 = 'TTbar70PU-memoryFile.fv3.clean.writeAll.recT.072617.bin'
+FILELarge = 'memoryFile.fv3.clean.writeAll.recT.072617.bin'
 
-# mkFit command formating
-CMD  = './mkFit/mkFit --cmssw-n2seeds --input-file {:s} --build-ce --num-thr {:d} --num-events {:d} --silent'
-
-# Tau command formatting
-# Grover
-# TAU = 'tau_exec -T 93948b87,tbb,papi,serial,icpc -ebs {:s}'
-# Talapas
-TAU = 'tau_exec -T 05c9fa46,tbb,papi,serial,icpc -ebs {:s}'
+SBATCH = "srun --partition=short --job-name=mkFit_{:d} --nodes=1 --ntasks-per-node=1 --cpus-per-task={:d} --time=1-00:00:00 "
 
 
-SBATCH = "srun --partition=short --job-name=mkFit_{:d} --nodes=1 --ntasks-per-node=1 --time=1-00:00:00 "
+PATH=('PATH', TAU_DIR + '/x86_64/bin')
 
-NUM_RUNS = 5 
-THREAD_LIST = [2, 4, 8, 16, 32, 48, 64, 96, 112, 128, 256]
+
+
+NUM_RUNS = 5
+THREAD_LIST = [1, 8, 16, 32, 48, 56]
 NUM_EVENTS=100
 
 env = dict(os.environ)
@@ -57,32 +59,39 @@ for var,val in ENV:
 
 env[PATH[0]] = PATH[1] + ':' + env[PATH[0]]
 
-EXP_LIST = ['manual_scaling_TTbar35_cache', 'manual_scaling_TTbar70_cache']
+if machine == 'Talapas':
+	PATH=('PATH', '/projects/hpcl/bgravell/mictest_sampling/mkFit/')
+	env[PATH[0]] = PATH[1] + ':' + env[PATH[0]]
+	LD_PATH = ('LD_LIBRARY_PATH', '/projects/hpcl/bgravell/mictest_sampling/')
+	env[LD_PATH[0]] = LD_PATH[1] + ':' + env[LD_PATH[0]]
 
-# GROVER
-'''
-METRIC_LIST = ['PAPI_TLB_DM', \
-'PAPI_BR_INS', 'PAPI_BR_CN', 'PAPI_BR_UCN', 'PAPI_BR_MSP',\
-'PAPI_NATIVE_UOPS_RETIRED:SCALAR_SIMD','PAPI_NATIVE_UOPS_RETIRED:PACKED_SIMD',\
-'PAPI_TOT_CYC','PAPI_TOT_INS',\
-'PAPI_L2_TCM', 'PAPI_L2_TCA',\
-'PAPI_RES_STL',\
-'PAPI_LST_INS', 'PAPI_L1_TCM',\
-'PAPI_NATIVE_LLC_REFERENCES','PAPI_NATIVE_LLC_MISSES']
-'''
-
-# Talapas
-METRIC_LIST = ['PAPI_TLB_DM', 'PAPI_TLB_IM', \
-'PAPI_BR_INS', 'PAPI_BR_CN', 'PAPI_BR_MSP',\
-'PAPI_SP_OPS', 'PAPI_DP_OPS', 'PAPI_VEC_SP', 'PAPI_VEC_DP', \
-'PAPI_TOT_CYC','PAPI_TOT_INS', 'PAPI_RES_STL',\
-'PAPI_L1_TCM', 'PAPI_L1_TCA',\
-'PAPI_L2_TCM', 'PAPI_L2_TCA',\
-'PAPI_L3_TCM', 'PAPI_L3_TCA',\
-'PAPI_LST_INS']
+EXP_LIST = ['manual_scaling_TTbar35_talapas', 'manual_scaling_TTbar70_talapas', 'manual_scaling_Large_talapas', \
+	'manual_scaling_TTbar35_talapas_fullnode', 'manual_scaling_TTbar70_talapas_fullnode', 'manual_scaling_Large_talapas_fullnode']
 
 
-def run_trial(exp_name, slurm=False):
+if machine == 'Grover':
+	METRIC_LIST = ['PAPI_TLB_DM', \
+	'PAPI_BR_INS', 'PAPI_BR_CN', 'PAPI_BR_UCN', 'PAPI_BR_MSP',\
+	'PAPI_NATIVE_UOPS_RETIRED:SCALAR_SIMD','PAPI_NATIVE_UOPS_RETIRED:PACKED_SIMD',\
+	'PAPI_TOT_CYC','PAPI_TOT_INS',\
+	'PAPI_L2_TCM', 'PAPI_L2_TCA',\
+	'PAPI_RES_STL',\
+	'PAPI_LST_INS', 'PAPI_L1_TCM',\
+	'PAPI_NATIVE_LLC_REFERENCES','PAPI_NATIVE_LLC_MISSES']
+
+
+
+if machine == 'Talapas':
+	METRIC_LIST = ['PAPI_TLB_DM', 'PAPI_TLB_IM', \
+	'PAPI_BR_INS', 'PAPI_BR_CN', 'PAPI_BR_MSP',\
+	'PAPI_SP_OPS', 'PAPI_DP_OPS', 'PAPI_VEC_SP', 'PAPI_VEC_DP', \
+	'PAPI_TOT_CYC','PAPI_TOT_INS', 'PAPI_RES_STL',\
+	'PAPI_L1_TCM', 'PAPI_LST_INS', \
+	'PAPI_L2_TCM', 'PAPI_L2_TCA',\
+	'PAPI_L3_TCM', 'PAPI_L3_TCA']
+
+
+def run_trial(exp_name, in_file, slurm=True, full_node=False):
 	'''
 	exp_name is the name of the mimiced taucmdr experiment
 	it will be used as the subdirectory under DIR
@@ -95,28 +104,79 @@ def run_trial(exp_name, slurm=False):
 
 	trial_count = 0
 
+	ntrials = len(METRIC_LIST) * len(THREAD_LIST) * NUM_RUNS
+	sub_procs = [None for x in range(ntrials)]
+
 	for m in METRIC_LIST:
 		env['TAU_METRICS'] =  'TIME,' + m
 
 		for n in THREAD_LIST:
-			for i in range(NUM_RUNS):
-				if(slurm):
-					sp.Popen(SBATCH.format(n) + TAU.format(CMD.format(FILE, n, NUM_EVENTS)), shell=True, env=env).wait()
-				else:
-					sp.Popen(TAU.format(CMD.format(FILE, n, NUM_EVENTS)), shell=True, env=env).wait()
-				trial_dir =  DIR + exp_name + '/' + str(trial_count)
-				sp.Popen('mkdir ' + trial_dir, shell=True, env=env).wait()
-				sp.Popen('mv MULTI__TIME ' + trial_dir + '/MULTI__TIME', shell=True, env=env).wait()
-                                sp.Popen('mv MULTI__' + m.replace(':', '_') + ' ' + trial_dir + '/MULTI__' + m.replace(':', '_'), shell=True, env=env).wait()
-				trial_count += 1
+				
+			if full_node:
+				ncores=28
+			else:
+				ncores=n/2+n%2
 
+			for i in range(NUM_RUNS):
+				
+				# build the commands to start trials
+				# make a new directory for results, go there, run new process
+				trial_dir =  DIR + exp_name + '/' + str(trial_count)
+				if slurm:
+					trial_cmd = SBATCH.format(n,ncores) + TAU.format(CMD.format(in_file, n, NUM_EVENTS))
+				else:
+					trial_cmd = TAU.format(CMD.format(in_file, n, NUM_EVENTS))
+				# its a little hacky but it'll have to do
+				cmd_li = ['mkdir ' + trial_dir, 'cd ' + trial_dir, 'cp /projects/hpcl/bgravell/mictest_sampling/Geoms/CMS-2017.so .', trial_cmd, 'rm CMS-2017.so']
+				cmd = build_cmd(cmd_li)
+
+				
+				if(slurm):
+					print cmd
+					sub_procs[trial_count] = sp.Popen(cmd, shell=True, env=env)
+				else:
+					sub_procs[trial_count] = sp.Popen(cmd, shell=True, env=env).wait()
+
+				trial_count += 1
 
 		print("\n\n")
 
-#run_trial(EXP_LIST[0])
-#run_trial(EXP_LIST[1])
-#run_trial('test')
-env['TAU_METRICS'] =  'TIME,PAPI_TOT_INS'
-print SBATCH.format(100) + TAU.format(CMD.format(FILE, 100, NUM_EVENTS))
-sp.Popen(SBATCH.format(100) + TAU.format(CMD.format(FILE, 100, NUM_EVENTS)), shell=True, env=env)
 
+	for proc in sub_procs:
+		proc.wait()
+
+
+def build_cmd(cmd_li):
+	s = cmd_li[0]
+	for c in range(len(cmd_li)-1):
+		s = s + '; ' + cmd_li[c+1]
+
+	return s
+
+run_trial(EXP_LIST[0],FILE_DIR+FILE35,SLURM)
+run_trial(EXP_LIST[1],FILE_DIR+FILE70,SLURM)
+NUM_EVENTS=4550
+run_trial(EXP_LIST[2],FILE_DIR+FILELarge,SLURM)
+
+NUM_EVENTS=100
+run_trial(EXP_LIST[3],FILE_DIR+FILE35,SLURM,True)
+run_trial(EXP_LIST[4],FILE_DIR+FILE70,SLURM,True)
+NUM_EVENTS=4550
+run_trial(EXP_LIST[5],FILE_DIR+FILELarge,SLURM,True)
+
+#run_trial('test',FILE_DIR+FILE35,SLURM)
+#run_trial('test',FILE_DIR+FILE35,SLURM)
+
+
+def test():
+	sp.Popen('rm -rf .tau/mictest_sampling/test/; mkdir .tau/mictest_sampling/test/', shell=True, env=env).wait()
+	env['TAU_METRICS'] =  'TIME,PAPI_TOT_INS'
+	cmd_li = ['mkdir .tau/mictest_sampling/test/0',\
+			 'cd .tau/mictest_sampling/test/0',\
+			 SBATCH.format(4,2) + TAU.format(CMD.format(FILE_DIR+FILE35, 4, NUM_EVENTS))]
+	cmd = build_cmd(cmd_li)
+	print cmd
+	p=sp.Popen(cmd, shell=True, env=env)
+	p.wait()
+
+#test()
