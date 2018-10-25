0b8e95aeacffd522735237a9a0d269966db86468
Brian J Gravelle <gravelle@cs.uoregon.edu>
2018-10-25 10:19:38 -0700

    modulo hack

--- Matriplex/MatriplexCommon.h
+++ Matriplex/MatriplexCommon.h
@@ -14,6 +14,8 @@
 
 #include "immintrin.h"
 
+#define mod(X, Y)  ((X) % (Y))
+
 #if defined(MPLEX_USE_INTRINSICS)
 
   #if defined(__MIC__) || defined(__AVX__) || defined(__AVX512F__)
@@ -71,52 +73,56 @@
 // #undef MPLEX_INTRINSICS
 
 // #endif
+ 
+#ifndef HACK_SIZE
+#define HACK_SIZE 100
+#endif
 
 namespace Matriplex
 {
    typedef int idx_t;
 
-   constexpr idx_t hacked_size = 100;
-   constexpr idx_t h0 = std::min(0,hacked_size);
-   constexpr idx_t h1 = std::min(1,hacked_size);
-   constexpr idx_t h2 = std::min(2,hacked_size);
-   constexpr idx_t h3 = std::min(3,hacked_size);
-   constexpr idx_t h4 = std::min(4,hacked_size);
-   constexpr idx_t h5 = std::min(5,hacked_size);
-   constexpr idx_t h6 = std::min(6,hacked_size);
-   constexpr idx_t h7 = std::min(7,hacked_size);
-   constexpr idx_t h8 = std::min(8,hacked_size);
-   constexpr idx_t h9 = std::min(9,hacked_size);
-   constexpr idx_t h10 = std::min(10,hacked_size);
-   constexpr idx_t h11 = std::min(11,hacked_size);
-   constexpr idx_t h12 = std::min(12,hacked_size);
-   constexpr idx_t h13 = std::min(13,hacked_size);
-   constexpr idx_t h14 = std::min(14,hacked_size);
-   constexpr idx_t h15 = std::min(15,hacked_size);
-   constexpr idx_t h16 = std::min(16,hacked_size);
-   constexpr idx_t h17 = std::min(17,hacked_size);
-   constexpr idx_t h18 = std::min(18,hacked_size);
-   constexpr idx_t h19 = std::min(19,hacked_size);
-   constexpr idx_t h20 = std::min(20,hacked_size);
-   constexpr idx_t h21 = std::min(21,hacked_size);
-   constexpr idx_t h22 = std::min(22,hacked_size);
-   constexpr idx_t h23 = std::min(23,hacked_size);
-   constexpr idx_t h24 = std::min(24,hacked_size);
-   constexpr idx_t h25 = std::min(25,hacked_size);
-   constexpr idx_t h26 = std::min(26,hacked_size);
-   constexpr idx_t h27 = std::min(27,hacked_size);
-   constexpr idx_t h28 = std::min(28,hacked_size);
-   constexpr idx_t h29 = std::min(29,hacked_size);
-   constexpr idx_t h30 = std::min(30,hacked_size);
-   constexpr idx_t h31 = std::min(31,hacked_size);
-   constexpr idx_t h32 = std::min(32,hacked_size);
-   constexpr idx_t h33 = std::min(33,hacked_size);
-   constexpr idx_t h34 = std::min(34,hacked_size);
-   constexpr idx_t h35 = std::min(35,hacked_size);
-   constexpr idx_t h36 = std::min(36,hacked_size);
-   constexpr idx_t h37 = std::min(37,hacked_size);
-   constexpr idx_t h38 = std::min(38,hacked_size);
-   constexpr idx_t h39 = std::min(39,hacked_size);
+   constexpr idx_t hacked_size = HACK_SIZE;
+   constexpr idx_t h0 = mod(0,hacked_size);
+   constexpr idx_t h1 = mod(1,hacked_size);
+   constexpr idx_t h2 = mod(2,hacked_size);
+   constexpr idx_t h3 = mod(3,hacked_size);
+   constexpr idx_t h4 = mod(4,hacked_size);
+   constexpr idx_t h5 = mod(5,hacked_size);
+   constexpr idx_t h6 = mod(6,hacked_size);
+   constexpr idx_t h7 = mod(7,hacked_size);
+   constexpr idx_t h8 = mod(8,hacked_size);
+   constexpr idx_t h9 = mod(9,hacked_size);
+   constexpr idx_t h10 = mod(10,hacked_size);
+   constexpr idx_t h11 = mod(11,hacked_size);
+   constexpr idx_t h12 = mod(12,hacked_size);
+   constexpr idx_t h13 = mod(13,hacked_size);
+   constexpr idx_t h14 = mod(14,hacked_size);
+   constexpr idx_t h15 = mod(15,hacked_size);
+   constexpr idx_t h16 = mod(16,hacked_size);
+   constexpr idx_t h17 = mod(17,hacked_size);
+   constexpr idx_t h18 = mod(18,hacked_size);
+   constexpr idx_t h19 = mod(19,hacked_size);
+   constexpr idx_t h20 = mod(20,hacked_size);
+   constexpr idx_t h21 = mod(21,hacked_size);
+   constexpr idx_t h22 = mod(22,hacked_size);
+   constexpr idx_t h23 = mod(23,hacked_size);
+   constexpr idx_t h24 = mod(24,hacked_size);
+   constexpr idx_t h25 = mod(25,hacked_size);
+   constexpr idx_t h26 = mod(26,hacked_size);
+   constexpr idx_t h27 = mod(27,hacked_size);
+   constexpr idx_t h28 = mod(28,hacked_size);
+   constexpr idx_t h29 = mod(29,hacked_size);
+   constexpr idx_t h30 = mod(30,hacked_size);
+   constexpr idx_t h31 = mod(31,hacked_size);
+   constexpr idx_t h32 = mod(32,hacked_size);
+   constexpr idx_t h33 = mod(33,hacked_size);
+   constexpr idx_t h34 = mod(34,hacked_size);
+   constexpr idx_t h35 = mod(35,hacked_size);
+   constexpr idx_t h36 = mod(36,hacked_size);
+   constexpr idx_t h37 = mod(37,hacked_size);
+   constexpr idx_t h38 = mod(38,hacked_size);
+   constexpr idx_t h39 = mod(39,hacked_size);
 
    inline void align_check(const char* pref, void *adr)
    {
